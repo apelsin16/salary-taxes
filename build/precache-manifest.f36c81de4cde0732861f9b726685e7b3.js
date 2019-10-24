ab4ac34215b9f53bf664a82ee88b3e51f4fcc143
Apelsin16 <apelsin@i.ua>
2019-10-24 09:07:52 +0300

    salary & prepayment ready

---
+++ build/precache-manifest.f36c81de4cde0732861f9b726685e7b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8fe25bd886d685374e1f153c815816",
+    "url": "/salary-taxes/build/index.html"
+  },
+  {
+    "revision": "6706098e0d7abe2336b5",
+    "url": "/salary-taxes/build/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "521dcbe2adc7d7d88c6e",
+    "url": "/salary-taxes/build/static/js/2.702c7d94.chunk.js"
+  },
+  {
+    "revision": "6706098e0d7abe2336b5",
+    "url": "/salary-taxes/build/static/js/main.384f8eb1.chunk.js"
+  },
+  {
+    "revision": "2093367edea3dee83434",
+    "url": "/salary-taxes/build/static/js/runtime-main.22c28f03.js"
+  }
+]);
