7be1c4ca20312b409d0bd37d6c811888a2c6202c
Apelsin16 <apelsin@i.ua>
2019-10-24 10:42:52 +0300

    salary & prepayment ready

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './components/app/App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render( 
     <Fragment>
         <CssBaseline />
-        <App />
+        <BrowserRouter basename="/salary-taxes/build">
+            <App />
+        </BrowserRouter>
     </Fragment>,        
     document.getElementById('root'));
